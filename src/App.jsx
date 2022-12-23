8e9a144c7ec80b293c81053e48e02b3b08804554
github_username <you@domain.com>
2022-12-23 17:31:18 +0100

    "link also"

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import Home from "./page/Home";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
 import UxCase from "./page/UxCase";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="container">
-      <Navbar />
-      <Routes>
-      <Route exact path="/*" element={<Home />}/>
-      <Route exact path="/uxcase" element={<UxCase />}/>
-      </Routes>
-      <Footer/>
-    </div>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/uxcase" element={<UxCase />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
